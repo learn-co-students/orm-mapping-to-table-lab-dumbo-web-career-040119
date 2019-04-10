08161194b4032f52fa70da9832e8ada5cd00d2ea
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-10 18:49:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -34,11 +34,14 @@ class Student
       values(?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create
-    
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
   
-  
+
 end
